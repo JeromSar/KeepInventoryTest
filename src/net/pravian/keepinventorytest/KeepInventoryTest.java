5807f4681cf02d1584189fe2b8f970e095435719
Jerom van der Sar <jerom.sar@hotmail.com>
2014-07-31 02:52:26 +0200

    Version 1.0

---
+++ src/net/pravian/keepinventorytest/KeepInventoryTest.java
@@ -0,0 +1,22 @@
+package net.pravian.keepinventorytest;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class KeepInventoryTest extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        super.getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        // Keeps inventory if player is OP
+        if (event.getEntity().isOp()) {
+            event.setKeepInventory(true);   
+        }
+    }
+}
